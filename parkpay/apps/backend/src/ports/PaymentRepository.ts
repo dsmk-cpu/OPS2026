2c5ca02fbccd64ac32d96f77b495e4057398a9f3
Christian Heinz <christian.heinz-2@mni.thm.de>
2026-09-22 21:24:00 +0200

    feat: adds payment repository port.

---
+++ parkpay/apps/backend/src/ports/PaymentRepository.ts
@@ -0,0 +1,10 @@
+import {Payment} from "../domain/Payment.js";
+import {PaymentStatus} from "../domain/PaymentStatus.js";
+
+export interface PaymentRepository {
+    save(payment: Payment): Promise<void>
+    findById(id: string): Promise<Payment| null>
+    findByIdempotencyKey(idempotencyKey: string): Promise<Payment | null>
+    findByParkingID(parkingId: number): Promise<Payment | null>
+    findByStatus(status: PaymentStatus): Promise<Payment[]>
+}
